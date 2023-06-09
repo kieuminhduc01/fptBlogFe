8ac58de4498be3868c8f7cc2187a4a9b1574e65f
bach <bach.tv2000@gmail.com>
2023-06-09 14:08:12 +0700

    add todo

--- type/urlPath.js
+++ type/urlPath.js
@@ -9,9 +9,10 @@ export const UrlPath = {
   detail: { url: '/detail/', title: 'Chi Tiết Bài Viết' },
   auth: { url: '/auth', title: 'Đăng nhập' },
 };
-export const Profile = {  // không fix cứng  mà call api
+export const Profile = {
   Fb: 'https://www.facebook.com/phuongwings?mibextid=LQQJ4d',
   In: 'https://www.linkedin.com/in/maiphuongnguyen0704/?fbclid=IwAR3HMgU5_wsNJjFQM-yW197zhdqHIbTUaYPxcXC2YbCB-Fyz8ACJaWmNFaQ',
   Zl: 'https://zalo.me/0336850714',
   Gm: '',
 };
+//TODO không fix cứng  mà call api
