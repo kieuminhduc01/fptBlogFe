8b76afbd31f9c6982d55a30549b700036989c45d
TranBach <traihonguyen2001@gmail.com>
2023-06-29 14:08:00 +0700

    done fix iphone

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,7 +1,5 @@
 import { BASE_URL } from '@/api/request';
 import {
-  DivStyled,
-  FooterTextstylde,
   PStyled
 } from '@/components/footer/styledComponent';
 import { HrStyled } from '@/components/header/styledComponent';
@@ -27,13 +25,13 @@ const Footer = () => {
     <>
       <div className="footer-bg ">
         <StatusAlert />
-        <DivStyled>
+        <div>
           <div className="d-flex justify-content-center">
             <div className="w-94pc w-93pc-sm w-90pc-md w-90pc-lg w-88pc-xl w-88pc-xxl">
               <div>
                 <div className="h-102px-xxl h-100px-xl h-80px-lg h-80px-md h-50px-sm h-40px "></div>
                 <div className="d-flex justify-content-center">
-                  <div className="footer-text-container">
+                  <div className="z-index-dropdown footer-bg">
                     <PStyled className=" fs-40px-xxl fs-38px-xl fs-36px-lg fs-34px-md fs-28px-sm fs-24px letter-spacing-3px-xl letter-spacing-2px-xl letter-spacing-1px-lg letter-spacing-1px-md mb-4px">
                       Theo dõi thêm về Phương nhé!
                     </PStyled>
@@ -52,32 +50,32 @@ const Footer = () => {
                 <div className="d-flex justify-content-around w-35pc-xxl w-34pc-xl w-400px-lg w-320px-md w-300px-sm w-270px">
                   <div className="d-flex">
                     <FacebookIcon />
-                    <FooterTextstylde
-                      className="me-2 me-md-0 fs-22px-xxl fs-20px-xl fs-20px-lg fs-16px-md fs-16px-sm fs-14px"
+                    <a
+                      className=" footer-text me-2 me-md-0 fs-22px-xxl fs-20px-xl fs-20px-lg fs-16px-md fs-16px-sm fs-14px"
                       id="link"
                       href={proFile?.facebook}
                       target="_blank"
                     >
                       FACEBOOK
-                    </FooterTextstylde>
+                    </a>
                   </div>
                   <div className="d-flex">
                     <LinkedinIcon />
-                    <FooterTextstylde
-                      className=" ms-1 fs-22px-xxl fs-20px-xl fs-20px-lg fs-16px-md fs-16px-sm fs-14px"
+                    <a
+                      className="footer-text ms-1 fs-22px-xxl fs-20px-xl fs-20px-lg fs-16px-md fs-16px-sm fs-14px"
                       id="link"
                       href={proFile?.linkedin}
                       target="_blank"
                     >
                       LINKEDIN
-                    </FooterTextstylde>
+                    </a>
                   </div>
                 </div>
               </div>
               <div className="h-102px-xxl h-100px-xl h-80px-lg h-80px-md h-50px-sm h-40px"></div>
             </div>
           </div>
-        </DivStyled>
+        </div>
       </div>
     </>
   );
--- components/footer/styledComponent.js
+++ components/footer/styledComponent.js
@@ -6,31 +6,3 @@ export const PStyled = styled.p`
   font-style: normal;
   color: #960c0c;
 `;
-export const DivStyled = styled.div`
-  .header-container {
-    background-color: #fff;
-    text-align: center;
-    position: fixed;
-    top: 0;
-    right: 0;
-    left: 0;
-  }
-  .content-container {
-    opacity: 1;
-    background-color: #fff;
-  }
-  .footer-text-container {
-    background-color: #f2f2f2;
-    width: fit-content;
-    z-index: 100;
-  }
-`;
-export const FooterTextstylde = styled.a`
-  font-family: 'Lexend', sans-serif;
-  font-weight: normal;
-  font-style: normal;
-  text-align: left;
-  letter-spacing: 1px;
-  margin-bottom: 0;
-  margin-top: 6px;
-`;
